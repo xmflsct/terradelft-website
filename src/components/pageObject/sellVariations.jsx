8aed812b2bb2ab06697332fa3f83e0b209fe1707
Zhiyuan Zheng <me@xmflsct.com>
2020-03-18 21:18:27 +0100

    ATB validation working
    With options as React state instead of constant

--- src/components/pageObject/sellVariations.jsx
+++ src/components/pageObject/sellVariations.jsx
@@ -1,15 +1,10 @@
 import React, { useState } from "react"
 import { Button, Form } from "react-bootstrap"
 import Select from "react-select"
-import { intersection, isEmpty, keys } from "lodash"
+import { findIndex, intersection, isEmpty, keys } from "lodash"
 
 const SellVariations = ({ variations }) => {
-  const variant = {
-    variation: {},
-    colour: {},
-    size: {},
-  }
-  const options = {
+  const cache = {
     variation: [],
     colour: [],
     size: [],
@@ -19,45 +14,30 @@ const SellVariations = ({ variations }) => {
     colour: [],
     size: [],
   }
-  var finalIndex
-  const [enableATB, updateEnableATB] = useState(false)
 
   // Create variations availability mapping
-  variations.forEach((d, index) => {
-    if (d.colour) {
-      if (!(d.colour.colour in variant["colour"])) {
-        variant["colour"][d.colour.colour] = []
-      }
-      variant["colour"][d.colour.colour].push(index)
-    }
-    if (d.size) {
-      if (!(d.size.size in variant["size"])) {
-        variant["size"][d.size.size] = []
-      }
-      variant["size"][d.size.size].push(index)
-    }
-    if (d.variation) {
-      if (!(d.variation.variation in variant["variation"])) {
-        variant["variation"][d.variation.variation] = []
-      }
-      variant["variation"][d.variation.variation].push(index)
-    }
-  })
-  keys(variant).forEach(k => {
-    keys(variant[k]).forEach((nk, i) => {
-      options[k][i] = {
-        allowed: variant[k][nk],
-        label: nk,
-        value: nk,
-        isDisabled: false,
-      }
-    })
+  variations.forEach((d, i) => {
+    keys(d)
+      .filter(k => ["variation", "colour", "size"].includes(k) && d[k] !== null)
+      .forEach(k => {
+        const index = findIndex(cache[k], ["label", d[k][k]])
+        index === -1
+          ? cache[k].push({
+              label: d[k][k],
+              value: [i],
+              isDisabled: false,
+            })
+          : cache[k][index].value.push(i)
+      })
   })
 
+  const [options] = useState(cache)
+  const [enableATB, setEnableATB] = useState(false)
+
   const handleChosen = (d, m) => {
-    m.action === "select-option" && (optionsChosen[m.name] = [...d.allowed])
+    m.action === "select-option" && (optionsChosen[m.name] = [...d.value])
     m.action === "clear" && (optionsChosen[m.name] = [])
-    updateEnableATB(false)
+    setEnableATB(false)
 
     const chosenKeys = keys(optionsChosen).filter(
       k => optionsChosen[k].length > 0
@@ -66,13 +46,13 @@ const SellVariations = ({ variations }) => {
       case 0:
         // All options are available
         keys(options).forEach(k => {
-          options[k].forEach((_, i) => (options[k][i]["isDisabled"] = false))
+          options[k].forEach((_, i) => (options[k][i].isDisabled = false))
         })
         break
       case 1:
         // Check chosen options
         options[chosenKeys[0]].forEach(
-          (_, i) => (options[chosenKeys[0]][i]["isDisabled"] = false)
+          (_, i) => (options[chosenKeys[0]][i].isDisabled = false)
         )
         // Check non-chosen options
         keys(options)
@@ -83,8 +63,7 @@ const SellVariations = ({ variations }) => {
             })
           })
 
-        finalIndex = optionsChosen[chosenKeys[0]]
-        finalIndex.length === 1 && updateEnableATB(true)
+        optionsChosen[chosenKeys[0]].length === 1 && setEnableATB(true)
         break
       case 2:
         // Check chosen options
@@ -102,35 +81,27 @@ const SellVariations = ({ variations }) => {
           optionsChosen[chosenKeys[1]]
         )
 
-        finalIndex = intersection(
-          optionsChosen[chosenKeys[0]],
-          optionsChosen[chosenKeys[1]]
-        )
-        // console.log("chosen 2: " + finalIndex)
-        if (finalIndex.length === 1) {
-          updateEnableATB(true)
-        }
+        intersection(optionsChosen[chosenKeys[0]], optionsChosen[chosenKeys[1]])
+          .length === 1 && setEnableATB(true)
         break
       case 3:
         chosenKeys.forEach(k => {
           const checkKeys = keys(options).filter(nk => nk !== k)
           checkMultiple(
-            variant[checkKeys[0]],
-            variant[checkKeys[1]],
-            optionsChosen[variant[checkKeys[0]]],
-            optionsChosen[variant[checkKeys[1]]]
+            checkKeys[0],
+            checkKeys[1],
+            optionsChosen[checkKeys[0]],
+            optionsChosen[checkKeys[1]]
           )
         })
 
-        // finalIndex = intersection(
-        //   intersection(
-        //     optionsChosen[chosenKeys[0]],
-        //     optionsChosen[chosenKeys[1]]
-        //   ),
-        //   optionsChosen[chosenKeys[2]]
-        // )
-        // console.log("chosen 3: " + finalIndex)
-        // finalIndex.length === 1 && changeEnabled(true)
+        intersection(
+          intersection(
+            optionsChosen[chosenKeys[0]],
+            optionsChosen[chosenKeys[1]]
+          ),
+          optionsChosen[chosenKeys[2]]
+        ).length === 1 && setEnableATB(true)
         break
       default:
         break
@@ -138,7 +109,7 @@ const SellVariations = ({ variations }) => {
   }
 
   const updateOption = (opt, ref) => {
-    opt["isDisabled"] = isEmpty(intersection(opt["allowed"], ref))
+    opt.isDisabled = isEmpty(intersection(opt.value, ref))
   }
   const checkMultiple = (variantA, variantB, allowedA, allowedB) => {
     const allowed = intersection(allowedA, allowedB)
@@ -154,34 +125,34 @@ const SellVariations = ({ variations }) => {
   return (
     <Form>
       <Form.Group>
-        {variant["variation"] && (
+        {options.variation && (
           <>
             <Form.Label>Variation</Form.Label>
             <Select
               name="variation"
-              options={options["variation"]}
+              options={options.variation}
               isClearable
               onChange={handleChosen}
             />
           </>
         )}
-        {variant["colour"] && (
+        {options.colour && (
           <>
             <Form.Label>Colour</Form.Label>
             <Select
               name="colour"
-              options={options["colour"]}
+              options={options.colour}
               isClearable
               onChange={handleChosen}
             />
           </>
         )}
-        {variant["size"] && (
+        {options.size && (
           <>
             <Form.Label>Size</Form.Label>
             <Select
               name="size"
-              options={options["size"]}
+              options={options.size}
               isClearable
               onChange={handleChosen}
             />
