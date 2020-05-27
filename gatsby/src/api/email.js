edaaeeebeb26216e49a5800e4273076bd1921437
Zhiyuan Zheng <me@xmflsct.com>
2020-05-27 22:12:10 +0200

    Use local dev url

--- gatsby/src/api/email.js
+++ gatsby/src/api/email.js
@@ -1,14 +1,22 @@
 import ky from 'ky-universal'
 
+const urlDevelopment = 'http://localhost:3000'
+const urlProduction = `https://${process.env.GATSBY_API_ENDPOINT}`
+
 export async function sendEmail (token, data) {
   try {
     return await ky
-      .post(`https://${process.env.GATSBY_API_ENDPOINT}/api/email`, {
-        json: {
-          token: token,
-          data: data
+      .post(
+        `${
+          process.env.NODE_ENV === 'production' ? urlProduction : urlDevelopment
+        }/api/email`,
+        {
+          json: {
+            token: token,
+            data: data
+          }
         }
-      })
+      )
       .json()
   } catch (error) {
     return error.response.json()
